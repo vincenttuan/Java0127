a77d0deeeb6c706a9a5dc410f644ee8b5f764874
MB-teacher <MB-teacher@192.168.1.157>
2021-01-27 16:35:04 +0800

    MTA

--- src/main/java/com/mta/case5/MultiArrayDemo2.java
+++ src/main/java/com/mta/case5/MultiArrayDemo2.java
@@ -9,7 +9,8 @@ public class MultiArrayDemo2 {
             {0, 80, 70}
         };
         // 如何將 0 變成 60
-        
+        scores[0][2] = 60;
+        scores[1][0] = 60;
         System.out.println(Arrays.toString(scores[0]));
         System.out.println(Arrays.toString(scores[1]));
     }
