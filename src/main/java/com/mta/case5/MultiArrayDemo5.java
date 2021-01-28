2a2c9faf35ec32ca650d79fe385f3f1e85a34dee
MB-teacher <MB-teacher@192.168.1.157>
2021-01-28 13:25:48 +0800

    MTA

---
+++ src/main/java/com/mta/case5/MultiArrayDemo5.java
@@ -0,0 +1,12 @@
+package com.mta.case5;
+
+public class MultiArrayDemo5 {
+    public static void main(String[] args) {
+        double[][] values = {
+            {160.5, 170, 180.5}, // 身高
+            {56, 60, 72}, // 體重
+        };
+        // 求這三人的 bmi 值 = ?
+        
+    }
+}
