d467b4d412b81aa576c60cdf7addab9e25527047
MB-teacher <MB-teacher@192.168.1.157>
2021-01-29 11:39:52 +0800

    MTA

--- src/main/java/com/mta/oo4/Main2.java
+++ src/main/java/com/mta/oo4/Main2.java
@@ -5,7 +5,22 @@ import java.util.ArrayList;
 
 public class Main2 {
     public static void main(String[] args) {
-        ArrayList<Student> students = getStudents(30);
-        System.out.println(students);
+        int n = 30;
+        ArrayList<Student> students = getStudents(n);
+        // System.out.println(students);
+        // 1.要印出未成年
+        for(Student student : students) {
+            if(!student.isAdult()) {
+                System.out.println(student);
+            }
+        }
+        // 2.成年與未成年的比例
+        int amountAdult = 0;
+        for(Student student : students) {
+            if(student.isAdult()) amountAdult++;
+        }
+        System.out.printf("已成年: %2d (%.1f)\n", amountAdult, (double)amountAdult/n);
+        System.out.printf("未成年: %2d (%.1f)\n", n-amountAdult, (double)(n-amountAdult)/n);
+        
     }
 }
