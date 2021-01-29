55eed20e3bbbcd647875f81ef77165806bf69b81
MB-teacher <MB-teacher@192.168.1.157>
2021-01-29 13:59:53 +0800

    MTA

--- src/main/java/com/mta/oo5/Util.java
+++ src/main/java/com/mta/oo5/Util.java
@@ -1,5 +1,41 @@
 package com.mta.oo5;
 
+import java.util.ArrayList;
+
+// 工具類別
 public class Util {
+    // 所有帳戶的集合資料
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    
+    // 開戶
+    public void addAccount(String no, int money) {
+        Account account = new Account(no, money);
+        accounts.add(account);
+    }
+    
+    // 提款
+    public void withdraw(String no, int money) {
+        
+    }
+    
+    // 存款
+    public void deposit(String no, int money) {
+        
+    }
+    
+    // 轉帳
+    public void deposit(String fromNo, String toNo, int money) {
+        
+    }
+    
+    // 查詢帳戶
+    public Account read(String no) {
+        return null;
+    }
+    
+    // 查詢全部帳戶
+    public ArrayList<Account> readAll() {
+        return accounts;
+    }
     
 }
