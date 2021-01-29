06c25274e82a99d34ffcd100606fef5c76f8761e
MB-teacher <MB-teacher@192.168.1.157>
2021-01-29 11:52:53 +0800

    MTA

--- src/main/java/com/mta/oo4/Main2.java
+++ src/main/java/com/mta/oo4/Main2.java
@@ -15,12 +15,21 @@ public class Main2 {
             }
         }
         // 2.成年與未成年的比例
-        int amountAdult = 0;
+        int amountAdult = 0; // 成年人數量
         for(Student student : students) {
             if(student.isAdult()) amountAdult++;
         }
-        System.out.printf("已成年: %2d (%.1f)\n", amountAdult, (double)amountAdult/n);
-        System.out.printf("未成年: %2d (%.1f)\n", n-amountAdult, (double)(n-amountAdult)/n);
+        int amountChild = n-amountAdult; // 未成年人數量
+        double percentAdult = (double)amountAdult/n; // 成年人比例
+        double percentChild = (double)amountChild/n; // 未成年人比例
+        // 長條圖表字串
+        String chartAdult = "";
+        String chartChild = "";
+        for(int i=0;i<(int)(percentAdult*10);i++) chartAdult += "*";
+        for(int i=0;i<(int)(percentChild*10);i++) chartChild += "*";
+        
+        System.out.printf("已成年: %2d (%.2f) %-10s\n", amountAdult, percentAdult, chartAdult);
+        System.out.printf("未成年: %2d (%.2f) %-10s\n", amountChild, percentChild, chartChild);
         
     }
 }
