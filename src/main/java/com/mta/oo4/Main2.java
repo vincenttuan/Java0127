e677d74590c2e48db59aa623d176e7d4820a1fbe
MB-teacher <MB-teacher@192.168.1.157>
2021-01-29 11:23:55 +0800

    MTA

--- src/main/java/com/mta/oo4/Main2.java
+++ src/main/java/com/mta/oo4/Main2.java
@@ -1,23 +1,11 @@
 package com.mta.oo4;
 
-import com.github.javafaker.Faker;
+import static com.mta.oo4.Util.getStudents;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main2 {
     public static void main(String[] args) {
-        ArrayList<Student> list = new ArrayList<>();
-        Faker faker = new Faker();
-        Random r = new Random();
-        for(int i=0;i<30;i++) {
-            Student student = new Student();
-            String name = faker.name().lastName();
-            int age = r.nextInt(50);
-            student.setName(name);
-            student.setAge(age);
-            // 將 student 放入 list 中
-            list.add(student);
-        }
-        System.out.println(list);
+        ArrayList<Student> students = getStudents();
+        System.out.println(students);
     }
 }
