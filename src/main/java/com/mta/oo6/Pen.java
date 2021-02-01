6c1f28bdb295b9eaeab8fede97446b64d58e0c52
MB-teacher <MB-teacher@192.168.1.157>
2021-02-01 09:49:00 +0800

    MTA

--- src/main/java/com/mta/oo6/Pen.java
+++ src/main/java/com/mta/oo6/Pen.java
@@ -1,15 +1,27 @@
 package com.mta.oo6;
 
 public class Pen {
-   String name; // (non-static)物件屬性
+   static String name = "鉛筆"; // (static)類別屬性
    int price;   // (non-static)物件屬性
    
-   void printName() { // (non-static)物件方法
+   static void printName() { // (non-static)物件方法
        System.out.printf("name: %s\n", name);
    }
    
    void printPrice() { // (non-static)物件方法
        System.out.printf("price: %s\n", price);
+   }
+   
+   static void printDM() {
+       Pen pen = new Pen();
+       pen.price = 10;
+       // 鉛筆 10 元起~
+       System.out.printf("%s %d 元起~\n", name, pen.price);
+   }
+   
+   static void printDM(Pen pen) {
+       // 鉛筆 10 元起~
+       System.out.printf("%s %d 元起~\n", name, pen.price);
    }
    
 }
--- src/main/java/com/mta/oo6/PenStore.java
+++ src/main/java/com/mta/oo6/PenStore.java
@@ -2,6 +2,19 @@ package com.mta.oo6;
 
 public class PenStore {
     public static void main(String[] args) {
+        Pen p1= new Pen();
+        Pen p2= new Pen();
+        
+        p2.name = "原子筆";
+        p1.price = 10;
+        p2.price = 20;
+        
+        Pen.printName();
+        p1.printPrice();
+        p2.printPrice();
+        
+        Pen.printDM();
+        Pen.printDM(p1);
         
     }
 }
