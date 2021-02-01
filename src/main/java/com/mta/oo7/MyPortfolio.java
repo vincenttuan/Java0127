4f598748b2e5c8b96ff2470ad8952aee3d5fd6c0
MB-teacher <MB-teacher@192.168.1.157>
2021-02-01 10:34:15 +0800

    MTA

--- src/main/java/com/mta/oo7/MyPortfolio.java
+++ src/main/java/com/mta/oo7/MyPortfolio.java
@@ -4,7 +4,8 @@ package com.mta.oo7;
 public class MyPortfolio {
     public static void main(String[] args) {
         MyStock s1 = new MyStock("台積電", "2330.TW", 550.5, 1000);
-        MyStock s2 = new MyStock("鴻海", "2317.TW", 200, 2000);
-        
+        MyStock s2 = new MyStock("鴻海", "2317.TW", 120, 2000);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
