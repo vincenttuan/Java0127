acc52a6f2c93527ea25db9797462ad583f4d9363
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 10:16:49 +0800

    MTA

--- src/main/java/com/mta/oo14_try/InputNumberDemo2.java
+++ src/main/java/com/mta/oo14_try/InputNumberDemo2.java
@@ -1,5 +1,6 @@
 package com.mta.oo14_try;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputNumberDemo2 {
@@ -8,9 +9,21 @@ public class InputNumberDemo2 {
         int days = 365;
         Scanner sc = new Scanner(System.in);
         System.out.printf("分子是 %d 請輸入分母: ", days);
-        int x = sc.nextInt();
-        int result = days/x;
-        System.out.printf("計算結果是: %d\n", result);
+        try {
+           int x = sc.nextInt();
+           int result = days/x;
+           System.out.printf("計算結果是: %d\n", result);
+           
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("請輸入數字~");
+            sc = new Scanner(System.in); // 重新取得 Scanner 物件
+        } catch (ArithmeticException e2) {
+            System.out.println(e2);
+            System.out.println("分母不可 = 0");
+            sc = new Scanner(System.in); // 重新取得 Scanner 物件
+        }
+        
         System.out.print("是否要繼續(Y/N)? ");
         String goon = sc.next();
         if(goon.equalsIgnoreCase("y")) {
