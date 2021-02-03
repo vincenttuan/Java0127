46daaa326d0ee0767fc720644961fceadfc50ce5
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 13:32:38 +0800

    MTA

--- src/main/java/com/mta/oo15_try/entity/User.java
+++ src/main/java/com/mta/oo15_try/entity/User.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mta.oo15_try.entity;
 
-/**
- *
- * @author MB-teacher
- */
+import java.util.Date;
+
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private Date tdate = new Date();
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getTdate() {
+        return tdate;
+    }
+
+    public void setTdate(Date tdate) {
+        this.tdate = tdate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + ", email=" + email + ", tdate=" + tdate + '}';
+    }
     
 }
