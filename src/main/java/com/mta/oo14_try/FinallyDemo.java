aa8c34ee20831a575f9bced36fe776440fa0b377
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 11:11:24 +0800

    MTA

---
+++ src/main/java/com/mta/oo14_try/FinallyDemo.java
@@ -0,0 +1,20 @@
+package com.mta.oo14_try;
+
+public class FinallyDemo {
+    public static void main(String[] args) {
+        String openFile = "開啟檔案";
+        System.out.printf("程式啟動: %s\n", openFile);
+        
+        try {
+            System.out.printf("計算結果: %d\n", 10/0);
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        } finally { // 不論 try 區塊內是否有錯誤發生, 皆會執行此方法
+            openFile = "關閉檔案";
+            System.out.printf("程式結束: %s\n", openFile);
+        }
+        
+        
+    }
+}
