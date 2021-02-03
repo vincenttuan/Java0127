face39b1331cca5e7222c8e012a3c405ce0d29f7
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 16:15:00 +0800

    MTA

--- src/main/java/com/mta/oo15_try/Main.java
+++ src/main/java/com/mta/oo15_try/Main.java
@@ -1,10 +1,62 @@
 package com.mta.oo15_try;
 
+import com.mta.oo15_try.exception.UserFailLoginException;
+import com.mta.oo15_try.exception.UserNotFoundException;
+import com.mta.oo15_try.service.UserService;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     public static void action(int n) {
-        
+        UserService us = new UserService();
+        Scanner sc = new Scanner(System.in);
+        String username = "";
+        String password = "";
+        String email = "";
+        switch(n) {
+            case 1:
+                us.print();
+                break;
+            case 2:
+                break;
+            case 3:
+                System.out.print("請輸入 username: ");
+                username = sc.next();
+                System.out.print("請輸入 password: ");
+                password = sc.next();
+                try {
+                    boolean isOK = us.getUserLogin().check(username, password);
+                    if(isOK) {
+                        System.out.print("請輸入 new password: ");
+                        String newPassword = sc.next();
+                        us.updatePassword(username, newPassword);
+                    }
+                } catch (UserFailLoginException ex) {
+                    System.out.println(ex.getMessage());
+                } catch (UserNotFoundException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                break;
+            case 4:
+                System.out.print("請輸入 username: ");
+                username = sc.next();
+                System.out.print("請輸入 password: ");
+                password = sc.next();
+                try {
+                    boolean isOK = us.getUserLogin().check(username, password);
+                    if(isOK) {
+                        System.out.print("請輸入 new Email: ");
+                        String newEmail = sc.next();
+                        us.updateEmail(username, newEmail);
+                    }
+                } catch (UserFailLoginException ex) {
+                    System.out.println(ex.getMessage());
+                } catch (UserNotFoundException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                break;
+        }
     }
     
     public static void menu() {
@@ -32,3 +84,4 @@ public class Main {
         menu();
     }
 }
+
