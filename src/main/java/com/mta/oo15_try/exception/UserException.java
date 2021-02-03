80ed96d516b81f0da73bb1f827d56276e294c720
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 13:47:52 +0800

    MTA

--- src/main/java/com/mta/oo15_try/exception/UserException.java
+++ src/main/java/com/mta/oo15_try/exception/UserException.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mta.oo15_try.exception;
 
-/**
- *
- * @author MB-teacher
- */
-public class UserException {
+public class UserException extends Exception {
+
+    public UserException(String string) {
+        super(string);
+    }
     
 }
---
+++ src/main/java/com/mta/oo15_try/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.mta.oo15_try.exception;
+
+public class UserNotFoundException extends UserException {
+    
+    public UserNotFoundException(String string) {
+        super(string);
+    }
+    
+    
+}
