8ec7e638295a9107374d14600e41982100b74694
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 14:30:37 +0800

    MTA

--- src/main/java/com/mta/oo15_try/service/UserService.java
+++ src/main/java/com/mta/oo15_try/service/UserService.java
@@ -1,5 +1,9 @@
 package com.mta.oo15_try.service;
 
+import com.mta.oo15_try.entity.User;
+import com.mta.oo15_try.exception.UserNotFoundException;
+import com.mta.oo15_try.util.UserUtil;
+
 public class UserService {
     private UserLogin userLogin = new UserLogin();
     
@@ -7,11 +11,19 @@ public class UserService {
         return userLogin;
     }
     
-    public void updatePassword(String username, String newPassword) {
-        
+    public void updatePassword(String username, String newPassword) throws UserNotFoundException {
+        // 是否有此人
+        User user = UserUtil.getUserByName(username);
+        // 修改 user's password
+        user.setPassword(newPassword);
+        System.out.println("User password 修改完畢");
     }
     
-    public void updateEmail(String username, String newEmail) {
-        
+    public void updateEmail(String username, String newEmail) throws UserNotFoundException {
+        // 是否有此人
+        User user = UserUtil.getUserByName(username);
+        // 修改 user's email
+        user.setEmail(newEmail);
+        System.out.println("User password 修改完畢");
     }
 }
