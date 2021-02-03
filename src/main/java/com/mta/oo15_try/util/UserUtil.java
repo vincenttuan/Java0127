6e56edea8e6402244ad61f8185cba7180243da5f
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 13:44:19 +0800

    MTA

--- src/main/java/com/mta/oo15_try/util/UserUtil.java
+++ src/main/java/com/mta/oo15_try/util/UserUtil.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mta.oo15_try.util;
 
-/**
- *
- * @author MB-teacher
- */
+import com.mta.oo15_try.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserUtil {
+    private static List<User> users;
+    static {
+        users = new ArrayList<>();
+        users.add(new User("john", "1234", "john@gmail.com"));
+        users.add(new User("mary", "5678", "mary@gmail.com"));
+        users.add(new User("helen", "0000", "helen@gmail.com"));
+    }
     
+    public static List<User> queryUsers() {
+        return users;
+    }
+    
+    public static User getUserByName(String username) {
+        User user = null;
+        for(User u : queryUsers()) {
+            if(u.getUsername().equals(username)) {
+                user = u;
+                break;
+            }
+        }
+        return user;
+    }
 }
