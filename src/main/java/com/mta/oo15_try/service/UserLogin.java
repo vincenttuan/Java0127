39a9bc829caa96d479103ff21374802d1d47adc0
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 14:32:45 +0800

    MTA

--- src/main/java/com/mta/oo15_try/service/UserLogin.java
+++ src/main/java/com/mta/oo15_try/service/UserLogin.java
@@ -6,19 +6,17 @@ import com.mta.oo15_try.exception.UserNotFoundException;
 import com.mta.oo15_try.util.UserUtil;
 
 class UserLogin {
-    public boolean check(String username, String password) throws UserFailLoginException {
-        try {
-            // 1. 是否有此人 ?
-            User user = UserUtil.getUserByName(username);
-            // 2. 比對 password
-            if(user.getPassword().equals(password)) {
-                System.out.println("登入成功");
-                return true;
-            }
-        } catch (UserNotFoundException ex) {
-            System.out.printf("登入失敗: %s\n", ex.getMessage());
-            return false; // 查無此人
+
+    public boolean check(String username, String password) throws UserFailLoginException, UserNotFoundException {
+        // 1. 是否有此人 ?
+        User user = UserUtil.getUserByName(username);
+        // 2. 比對 password
+        if (user.getPassword().equals(password)) {
+            System.out.println("登入成功");
+            // 3. 登入成功
+            return true; 
         }
+        // 3. 登入失敗
         UserFailLoginException e = new UserFailLoginException("登入失敗: 密碼錯誤");
         throw e;
     }
