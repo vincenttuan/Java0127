1868a91be653623ac2a251b62082ef62fa79573a
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 15:36:18 +0800

    MTA

--- src/test/java/oo15_test/Test.java
+++ src/test/java/oo15_test/Test.java
@@ -3,19 +3,22 @@ package oo15_test;
 import com.mta.oo15_try.exception.UserFailLoginException;
 import com.mta.oo15_try.exception.UserNotFoundException;
 import com.mta.oo15_try.service.UserService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Test {
     UserService us = new UserService();
     
+    // 測試所有 User 資料
     @org.junit.Test
     public void t1() {
-        // 測試所有 User 資料
         System.out.println("測試所有 User 資料:");
         System.out.println(us.queryUsers());
     }
+    
+    // 測試登入
     @org.junit.Test
     public void t2() {
-        // 測試登入
         String username = "john";
         String password = "1234";
         System.out.printf("測試登入 %s / %s:\n", username, password);
@@ -27,5 +30,19 @@ public class Test {
         } catch (UserNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+    
+    // 測試修改密碼
+    @org.junit.Test
+    public void t3() {
+        String username = "john";
+        String newPassword = "1111";
+        System.out.printf("將 %s 的 密碼改為 %s\n", username, newPassword);
+        try {
+            us.updatePassword(username, newPassword);
+        } catch (UserNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        t1(); // 查詢所有 User 資料
     }
 }
