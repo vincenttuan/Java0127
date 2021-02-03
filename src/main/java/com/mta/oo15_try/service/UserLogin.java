fc47beab6ba28f32eef0086bacc5a971689c8526
MB-teacher <MB-teacher@192.168.1.157>
2021-02-03 14:10:19 +0800

    MTA

--- src/main/java/com/mta/oo15_try/service/UserLogin.java
+++ src/main/java/com/mta/oo15_try/service/UserLogin.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mta.oo15_try.service;
 
-/**
- *
- * @author MB-teacher
- */
+import com.mta.oo15_try.entity.User;
+import com.mta.oo15_try.exception.UserNotFoundException;
+import com.mta.oo15_try.util.UserUtil;
+
 public class UserLogin {
-    
+    public boolean check(String username, String password) {
+        try {
+            // 1. 是否有此人 ?
+            User user = UserUtil.getUserByName(username);
+            // 2. 比對 password
+            if(user.getPassword().equals(password)) {
+                System.out.println("登入成功");
+                return true;
+            }
+        } catch (UserNotFoundException ex) {
+            System.out.printf("登入失敗: %s\n", ex.getMessage());
+            return false; // 查無此人
+        }
+        System.out.println("登入失敗: 密碼錯誤");
+        return false; // 密碼錯誤
+    }
 }
