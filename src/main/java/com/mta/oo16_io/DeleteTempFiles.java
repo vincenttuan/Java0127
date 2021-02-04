24807ec058fa9e0bab30beeb3e78847378509a1c
MB-teacher <MB-teacher@192.168.1.157>
2021-02-04 14:16:52 +0800

    MTA

---
+++ src/main/java/com/mta/oo16_io/DeleteTempFiles.java
@@ -0,0 +1,24 @@
+package com.mta.oo16_io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DeleteTempFiles {
+
+    public static void main(String[] args) {
+        // 指定一個目錄
+        File dir = new File("src\\main\\java\\com\\mta\\oo16_io\\files");
+        if(dir.isDirectory()) {
+            String[] fileNames = dir.list(); // 取得該路徑下所有的名稱(檔名or資料夾名)
+            for(String fname : fileNames) {
+                File f = new File(dir.getPath() + "/" + fname);
+                System.out.println(f);
+                if(f.isFile() && fname.contains("temp")) {
+                    f.delete();
+                }
+            }
+        } else {
+            System.out.printf("%s 不是目錄\n", dir);
+        }
+    }
+}
