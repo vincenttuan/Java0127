74a03498db2c7143e3ceb19e481768022ede5fae
MB-teacher <MB-teacher@192.168.1.157>
2021-02-04 15:29:57 +0800

    MTA

---
+++ src/main/java/com/mta/oo16_io/ReadFileDemo.java
@@ -0,0 +1,20 @@
+package com.mta.oo16_io;
+
+import java.io.FileReader;
+
+public class ReadFileDemo {
+    public static void main(String[] args) {
+        
+        try(FileReader fr = new FileReader("src\\main\\java\\com\\mta\\oo16_io\\files\\Hello.txt");) {
+            int data = 0;
+            String message = "";
+            while ((data = fr.read()) != -1) {
+                System.out.println(data + " --> " + (char)data);
+                message += (char)data;
+            }
+            System.out.println(message);
+        } catch (Exception e) {
+        }
+        
+    }
+}
