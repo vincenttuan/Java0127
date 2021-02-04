d50d6017fc2ed96851738b7765c61a40f499d11f
MB-teacher <MB-teacher@192.168.1.157>
2021-02-04 13:29:56 +0800

    MTA

---
+++ src/main/java/com/mta/oo16_io/CreateHelloFile.java
@@ -0,0 +1,10 @@
+package com.mta.oo16_io;
+
+import java.io.File;
+
+public class CreateHelloFile {
+    public static void main(String[] args) {
+        String fp = "src\\main\\java\\com\\mta\\oo16_io\\files\\Hello.txt";
+        File f = new File("");
+    }
+}
