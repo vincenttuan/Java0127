d1ab3f4e68cdf7868c6c05761f22f64781bf4949
MB-teacher <MB-teacher@192.168.1.157>
2021-02-05 10:23:38 +0800

    MTA

--- src/main/java/com/mta/oo17_io/Util.java
+++ src/main/java/com/mta/oo17_io/Util.java
@@ -1,5 +1,33 @@
 package com.mta.oo17_io;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import org.json.JSONObject;
+
 public class Util {
-    
+    private static final String ID = "fcc57465b76d35357c84e4e30fe2431a";
+    private static final String URL_STRING = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s";
+    public Weather getWeather(String country, String cityName) {
+        String q = cityName + "," + country;
+        String urlstr = String.format(URL_STRING, q, ID);
+        String jsonstr = "";
+        Weather weather = new Weather();
+        try {
+            Reader r = new InputStreamReader(new URL(urlstr).openStream());
+            int data = 0;
+            while((data = r.read()) != -1){
+                jsonstr += (char)data;
+            }
+            // 分析 json
+            JSONObject root = new JSONObject(jsonstr);
+            // 將 json 的資料配置到 weather 的物件屬性內
+            weather.setCountry(root.getJSONObject("sys").getString("country"));
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return weather;
+    }
 }
