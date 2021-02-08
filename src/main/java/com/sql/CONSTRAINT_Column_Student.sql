09fc101a2672d85b88f745705346c7e74c971b3b
MB-teacher <MB-teacher@192.168.1.157>
2021-02-08 12:03:15 +0800

    MTA

--- src/main/java/com/sql/CONSTRAINT_Column_Student.sql
+++ src/main/java/com/sql/CONSTRAINT_Column_Student.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE APP.STUDENT
--- ADD CONSTRAINT classroom_id_FK
--- Foreign Key (classroom_id) REFERENCES Classroom (id);
+ALTER TABLE APP.STUDENT
+ADD CONSTRAINT classroom_id_FK
+Foreign Key (classroom_id) REFERENCES Classroom (id);
 
-ALTER TABLE APP.STUDENT DROP CONSTRAINT classroom_id_FK
+--ALTER TABLE APP.STUDENT DROP CONSTRAINT classroom_id_FK
