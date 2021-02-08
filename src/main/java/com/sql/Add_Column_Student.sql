96aec7c4ed9b0f36cba00d0b2b587e2a4f540206
MB-teacher <MB-teacher@192.168.1.157>
2021-02-08 11:59:01 +0800

    MTA

---
+++ src/main/java/com/sql/Add_Column_Student.sql
@@ -0,0 +1,4 @@
+-- 增加欄位
+ALTER TABLE APP.STUDENT
+ADD COLUMN classroom_id INTEGER
+
---
+++ src/main/java/com/sql/CONSTRAINT_Column_Student.sql
@@ -0,0 +1,5 @@
+-- ALTER TABLE APP.STUDENT
+-- ADD CONSTRAINT classroom_id_FK
+-- Foreign Key (classroom_id) REFERENCES Classroom (id);
+
+ALTER TABLE APP.STUDENT DROP CONSTRAINT classroom_id_FK
