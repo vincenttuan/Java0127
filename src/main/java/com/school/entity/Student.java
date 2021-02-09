0031578abbeadd06941750320523d5a013c1cff6
MB-teacher <MB-teacher@192.168.1.157>
2021-02-09 09:47:28 +0800

    MTA

--- src/main/java/com/school/entity/Student.java
+++ src/main/java/com/school/entity/Student.java
@@ -1,5 +1,54 @@
 package com.school.entity;
 
+import java.sql.Timestamp;
+
 public class Student {
+    private int id;
+    private String name;
+    private int score;
+    private Timestamp ts;
+    // 欄位 classroom_id 所關聯的 classroom
+    // 多對一
+    private Classroom classroom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Timestamp getTs() {
+        return ts;
+    }
+
+    public void setTs(Timestamp ts) {
+        this.ts = ts;
+    }
+
+    public Classroom getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+    }
     
 }
