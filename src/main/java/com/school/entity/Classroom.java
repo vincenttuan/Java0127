e2935dc33d50c7fb291dd16b7086e739bb67e2e7
MB-teacher <MB-teacher@192.168.1.157>
2021-02-09 09:44:19 +0800

    MTA

--- src/main/java/com/school/entity/Classroom.java
+++ src/main/java/com/school/entity/Classroom.java
@@ -1,5 +1,36 @@
 package com.school.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Classroom {
+    private int id;
+    private String name;
+    // 一對多
+    private List<Student> students = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
     
 }
