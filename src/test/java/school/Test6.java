c8ae8f0587b84794c3065861f08f33e96027365f
MB-teacher <MB-teacher@192.168.1.157>
2021-02-10 09:36:09 +0800

    MTA

--- src/test/java/school/Test6.java
+++ src/test/java/school/Test6.java
@@ -1,8 +1,9 @@
 package school;
 
 import com.school.entity.Classroom;
-import com.school.util.DB;
+import com.school.entity.Student;
 import static com.school.util.DB.queryClassroom;
+import static com.school.util.DB.queryStudentByClassroomId;
 import java.util.List;
 import org.junit.Test;
 
@@ -10,6 +11,16 @@ public class Test6 {
     @Test
     public void t6() {
         List<Classroom> classrooms = queryClassroom();
+        // 裝配 student 前
+        for(Classroom room : classrooms) {
+            System.out.println(room);
+        }
+        // 裝配 student
+        for(Classroom room : classrooms) {
+            List<Student> students = queryStudentByClassroomId(room.getId());
+            room.setStudents(students); // 裝配
+        }
+        // 裝配 student 後
         for(Classroom room : classrooms) {
             System.out.println(room);
         }
