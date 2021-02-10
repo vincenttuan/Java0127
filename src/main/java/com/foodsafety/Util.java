56ce37c37220a447104eff40b5b335eb22e4d00d
MB-teacher <MB-teacher@192.168.1.157>
2021-02-10 13:56:51 +0800

    MTA

--- src/main/java/com/foodsafety/Util.java
+++ src/main/java/com/foodsafety/Util.java
@@ -5,8 +5,19 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class Util {
-    public static Rice[] queryRices() {
+    public static Rice[] queryBadRices() {
         String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        try {
+            URL url = new URL(path);
+            String json = new Scanner(url.openStream(), "utf-8").useDelimiter("\\A").next();
+            Rice[] rices = new Gson().fromJson(json, Rice[].class);
+            return rices;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    public static Rice[] queryGoodRices() {
+        String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx";
         try {
             URL url = new URL(path);
             String json = new Scanner(url.openStream(), "utf-8").useDelimiter("\\A").next();
