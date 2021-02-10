a5aff08f8fe78fd45ba42a4ac1a2afd24067d1c9
MB-teacher <MB-teacher@192.168.1.157>
2021-02-10 13:51:43 +0800

    MTA

--- src/main/java/com/foodsafety/Main.java
+++ src/main/java/com/foodsafety/Main.java
@@ -2,14 +2,17 @@ package com.foodsafety;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "日本";
+        String name = "池上";
         Rice[] rices = Util.queryRices();
         System.out.printf("%10s %10s %10s %s\n", "品名", "檢驗結果", "行政處分", "不合格原因");
         System.out.println("-----------------------------------------------------");
         for(Rice r : rices) {
             if(r.get品名().contains(name)) {
                 System.out.printf("%-10s %-10s %-10s %s\n", 
-                        r.get品名(), r.get檢驗結果(), r.get行政處分(), r.get不合格原因());
+                        r.get品名().replace("\n", ""), 
+                        r.get檢驗結果().replace("\n", ""), 
+                        r.get行政處分().replace("\n", ""), 
+                        r.get不合格原因().replace("\n", ""));
             }
         }
     }
